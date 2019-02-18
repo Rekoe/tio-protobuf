0089bc45f6e52a22fe93ccdc9d76a95d4279c09c
rekoe <koukou890@qq.com>
2019-02-18 11:11:37 +0800

    增加心跳机制

--- src/main/java/com/onemena/game/custom/handler/TextMessageHandler.java
+++ src/main/java/com/onemena/game/custom/handler/TextMessageHandler.java
@@ -6,12 +6,12 @@ import com.onemena.game.annotation.HandlerMapping;
 import com.onemena.game.proto.TextMessage;
 
 /**
- * @author nonpool
  * @version 1.0
  * @since 2018/1/29
  */
 @HandlerMapping("TextMessage")
 public class TextMessageHandler extends AbstractDataHandler<TextMessage> {
+
     @Override
     public void onEvent(TextMessage textMessage, ChannelContext ctx) throws Exception {
 
